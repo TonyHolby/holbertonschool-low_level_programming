7fe1a21c2f7fe1be0694e611dc684ffd5c4a3aec
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-19 13:06:04 +0000

    a C program that prints exactly : Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * the function puts - This is a description
+ * function main - prints exactly "Programming is like building a multilingual puzzle
+ * return: 0
  */
 int main(void)
 {
