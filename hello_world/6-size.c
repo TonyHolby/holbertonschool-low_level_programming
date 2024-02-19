9558e594b55aba9cd534895d47174e40c330c66b
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-19 14:34:36 +0000

    a C program that prints the size of various types on the computer it is compiled and run on

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the size of various types on the computer it is
+ * compiled and run on
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("int=%lu bytes\n", sizeof(int));
+	return (0);
+}
