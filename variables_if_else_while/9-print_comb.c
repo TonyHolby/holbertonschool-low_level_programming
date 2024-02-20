7fab71ca5d915fb0146d40caba5776bb6fa78c26
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-20 16:02:10 +0000

    Write a program that prints all possible combinations of single-digit numbers separeted by ,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		if (n !=  9)
+			putchar(n);
+			putchar(', ');
+		else
+			putchar(n);
+			putchar('\n');
+	}
+
+	return (0);
+}
