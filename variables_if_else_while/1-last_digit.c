a16d45415d6469b16a73eddfce687d33908473f3
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-20 10:34:10 +0000

    This program will assign a random number to the variable n each time it is executed, preceded by Last digit of and print is positive if n greater than 0, is zero if n is 0 and is negative if n is less than 0

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,23 +14,28 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_digit = n%10;
 
-	if (n <= 5)
+	if (last_digit > 5)
 	{
 		printf("%d Last digit of", n);
-		printf("and is greater than 5\n");
+		printf("is", last_digit);
+		printf ("and is greater than 5\n");
 	}
-	else if (n == 0)
+	else if (last_digit == 0)
 	{
 		printf("%d Last digit of", n);
+		printf("is", last_digit);
 		printf("and is 0\n");
 	}
 	else
 	{
 		printf("%d Last digit of", n);
+		printf("is", last_digit);
 		printf("and is less than 6 and not 0\n");
 	}
 
