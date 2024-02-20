e511c9ca2a206afdb796686049a788c0aa36bb09
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-20 13:05:14 +0000

    fix code

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,8 @@
 /**
  * main - Entry point
  *
- * Description: prints the alphabet in lowercase
+ * Description: prints the alphabet in lowercase and then in 
+ * uppercase, followed by a newline
  *
  * Return: 0
  */
