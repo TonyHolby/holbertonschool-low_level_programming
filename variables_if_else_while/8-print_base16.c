c23d0105cb0d03bd72be0434b92ad18bd4203493
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-20 15:21:51 +0000

    Write a program that prints all single digit numbers of base 16, followed by a new line - with the putchar function

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the numbers of base 16, followed by
+ * a new line with the putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar('0' + (n % 10));
+
+	for (c = 'a' ; n < 'g' ; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
