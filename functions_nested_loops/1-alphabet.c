7c75417fb0becb2297514a661fb3a8f929d38ada
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-22 15:37:37 +0000

    task_1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef string_PUTCHAR_H
-#define string_PUTCHAR_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
