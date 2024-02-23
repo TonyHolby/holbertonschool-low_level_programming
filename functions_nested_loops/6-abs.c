a1d995c5792a6e948ac39eb2b09053e32d31639b
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-23 11:13:52 +0000

    Write a function that prints the sign of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Entry point
+ *
+ * Description: a function that computes the absolute value of an integer
+ * @int: variable
+ *
+ * Return: abs(int)
+ */
+
+int _abs(int)
+{
+	return (int);
+}
