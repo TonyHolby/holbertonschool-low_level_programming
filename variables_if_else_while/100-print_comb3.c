3d16a325301281ad7e613be775be474f480d6943
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-25 21:41:26 +0000

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Write a program that prints all possible different
+ * combinations of two digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int increment_num2 = 1;
+
+	for (num1 = 0 ; num1 <= 8 ; num1++)
+	{
+		for (num2 = 1 ; num2 <= 9; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			putchar(',');
+			putchar(' ');
+
+			if (num2 == 9)
+			{
+				num2 = num2 + increment_num2;
+			}
+		}
+	putchar('\n');
+	}
+
+	return (0);
+}
