989c1c3e21737ce54376646bcf3a7cafe542dc3c
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-25 21:44:14 +0000

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -20,8 +20,8 @@ int main(void)
 	{
 		for (num2 = 1 ; num2 <= 9; num2++)
 		{
-			putchar(num1);
-			putchar(num2);
+			putchar('0' + num1);
+			putchar('0' + num2);
 			putchar(',');
 			putchar(' ');
 
