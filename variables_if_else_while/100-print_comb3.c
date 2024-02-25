5af50f5e2d181463ff953d1a3efc983af3cf0268
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-25 22:44:46 +0000

    100-print_comb3.c.

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -14,22 +14,15 @@ int main(void)
 {
 	int num1;
 	int num2;
-	int increment_num2 = 1;
 
 	for (num1 = 0 ; num1 <= 8 ; num1++)
-	{
-		num2 = num2 + increment_num2;
-
-		for (num2 = 1 ; num2 <= 9; num2++)
+	{	
+		for (num2 = 1 ; num2 <= 9 ; num2++)
 		{
 			putchar('0' + num1);
 			putchar('0' + num2);
-
-			while (num1 != 8 && num2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
