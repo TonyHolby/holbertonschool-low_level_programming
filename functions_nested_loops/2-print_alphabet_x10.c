dea599f9ab56d3e6059b3bcec579e410b352c696
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-25 14:06:09 +0000

    code improvement

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -17,7 +17,7 @@ void print_alphabet_x10(void)
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
-		_putchar(alphabet);
+			_putchar(alphabet);
 
 		_putchar('\n');
 	}
--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,16 +1,15 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _abs - Entry point
  *
  * Description: a function that computes the absolute value of an integer
- * @AN_INTEGER: variable
+ * @n: variable
  *
- * Return: abs(AN_INTEGER)
+ * Return: abs(n)
  */
 
-int _abs(int AN_INTEGER)
+int _abs(int n)
 {
-	return (abs(AN_INTEGER));
+	return (abs(n));
 }
