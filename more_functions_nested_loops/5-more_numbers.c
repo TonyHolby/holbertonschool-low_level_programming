a19e996f8d673ac887f5ec4b5090ee28d7a1ed15
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-26 12:49:56 +0000

    Write a function that prints a triangle, followed by a new line

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: a function that prints 10 times the numbers, from 0 to 14,
+ * followed by a new line
+ *
+ */
+
+void more_numbers(void)
+{
+	int num, i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (num = 0 ; num <= 14 ; num++)
+		{
+
+			if (num > 9)
+			{
+                                _putchar('0' + (num / 10));
+			}
+
+			_putchar('0' + (num % 10));
+		}
+		_putchar('\n');
+	}
+}
