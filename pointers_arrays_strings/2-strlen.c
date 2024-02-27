2df3f413d886f779c57748de8713acd298750f1a
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-27 16:31:03 +0000

    Save code

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * Description: a function that swaps the values of two integers
+ * @s: string
+ *
+ */
+
+int _strlen(char *s)
+{
