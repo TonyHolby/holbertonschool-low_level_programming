8273ba49f0e4707e55703f32f70bc47ad0ed8b73
TonyHolby <tonynemoutheholberton@gmail.com>
2024-02-29 22:56:40 +0000

    save code

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ *
+ * Description: a function that prints a string, in reverse,
+ * followed by a new line
+ * @s: string
+ *
+ * Return: the length of the string
+ */
+
+int _atoi(char *s)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		length++;
+	}
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (s == i)
+		{
+			m
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
