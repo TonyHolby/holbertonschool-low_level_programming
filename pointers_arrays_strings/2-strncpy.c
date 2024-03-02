341db4d5d3fa67e14b8484597a0a50988f6976ad
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-02 18:30:38 +0000

    Write a function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,33 +8,23 @@
  * @src: string
  * @n: integer
  *
- * Return: a pointer
+ * Return: the pointer dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
 	int c = 0;
-	int length_src = 0;
-	int length_dest = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		length_src++;
-	}
-
-	for (j = 0; dest[j] != '\0'; j++)
-	{
-		length_dest++;
-	}
-
-	for (c = 0; c < n; c++)
+	for (c = 0; c < n && src[c] != '\0'; c++)
 	{
 		dest[c] = src[c];
 	}
-
-	dest[n] = '\0';
+	
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
 
 	return (dest);
 }
