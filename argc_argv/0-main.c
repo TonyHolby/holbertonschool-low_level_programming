ebcb672cef7f1ee045da2152079fd5eea044eaff
TonyHolby <tonynemouthholberton@gmail.com>
2024-03-07 19:34:00 +0000

    Write a program that prints its name, followed by a new line

---
+++ argc_argv/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *name = "A_new_name_to_test";
+
+	printf("\n", name);
+	return (0);
+}
---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints its name, followed by a new line
+ * @argc: integer
+ * @argv: array
+ *
+ * Return: an integer
+ */
+
+int main(int argc, char **argv)
+{
+        while(argc--)
+                printf("%s\n", *argv++);
+        exit(EXIT_SUCCESS);
+}
