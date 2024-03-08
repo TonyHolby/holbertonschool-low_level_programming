8fb04e37a5c72334cd808e30734d936406c3743d
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-08 12:48:43 +0000

    update the readme file

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Write a program that multiplies two numbers
+ * @argc: integer
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int number1, number2, res;
+
+	if (
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[2]);
+
+	res = number1 + number2;
+	printf("%d\n", res);
+
+	return (0);
+}
