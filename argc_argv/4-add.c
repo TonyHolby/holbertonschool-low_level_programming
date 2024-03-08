52dd7d18b7b9060864008956d0cf9858518b7447
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-08 18:42:14 +0000

    Fix code

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -24,7 +24,11 @@ int main(int argc, char **argv)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (isdigit(argv[i][j]))
+			{
+				continue;
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
