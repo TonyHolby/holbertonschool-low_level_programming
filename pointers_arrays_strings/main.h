a4301e9e77d4047cbc5a55be3abfbb430b754bcd
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-09 16:04:25 +0000

    add the main.h file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 
 int _putchar(char c);
 char *cap_string(char *);
+char *string_toupper(char *string);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
