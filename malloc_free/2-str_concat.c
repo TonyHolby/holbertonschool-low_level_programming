a7cab5d6b398c8d147458d49c47c4678c0e3369e
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-12 12:07:11 +0000

    Fix code

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,42 +20,33 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		length_s1++;
 	}
-
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		length_s2++;
 	}
-
 	c = malloc(((length_s1 + length_s2) + 1) * sizeof(char));
-
 	if (c == NULL)
 	{
 		return (NULL);
 	}
-
 	while (k < length_s1)
 	{
 		c[k] = s1[k];
 		k++;
 	}
-
 	while (l < length_s2)
 	{
 		c[length_s1 + l] = s2[l];
 		l++;
 	}
-
 	c[length_s1 + length_s2] = '\0';
-
 	return (c);
 }
