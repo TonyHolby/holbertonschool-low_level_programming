b5d799d84d403825e98df25792f369c6f0e7f42f
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-12 12:50:20 +0000

    Fix code

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Entry point
+ *
+ * Description: Write a function that returns a pointer to a 2 dimensional
+ * array of integers
+ * @width: integer
+ * @height: integer
+ *
+ * Return: NULL if width or height = 0, else a pointer to a 2 dimensionnal
+ * array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, k;
+	int **grid = NULL;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc((width + height) * sizeof(char));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc((width + height) * sizeof(char));
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	
+		for (k = 0; k < width; k++)
+		{
+			grid[i][k] = 0;
+		}
+	}
+
+	return (grid);
+}
