114acbe97d9d3f651c409ff57850186fcb260f62
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-12 21:39:35 +0000

    Write a function that concatenates two strings

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Entry point
+ *
+ * Description: Write a function that concatenates two strings
+ * @s1: string
+ * @s2: string
+ * @n: unsigned integer
+ *
+ * Return: a pointer which contains s1, followed by the first n bytes of s2,
+ * and null terminated
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, length_s1 = 0, length_s2 = 0;
+	char *pointer;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	
+	for (i = 0; i != '\0'; i++)
+	{
+		length_s1++;
+	}
+	for (j = 0; j != '\0'; j++)
+	{
+		length_s2++;
+	}
+	
+	if (n >= length_s2)
+	{
+		n = length_s2;
+	}
+
+	pointer = malloc(length_s1 + (n + 1));
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < n; k++)
+	{
+		pointer[length_s1 + k] = s2[k];
+	}
+	pointer[length_s1 + n] = '\0';
+	
+	return (pointer);
+}
