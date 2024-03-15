b7d56b05cdf59bd8cf11d8707a6bc1b805a20b68
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-15 13:53:54 +0000

    Fix code

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	unsigned int *allocated_memory = NULL;
+	char *allocated_memory = NULL;
 
 	if (nmemb == 0 || size == 0)
 	{
