919bbc20e197cde0718c54f88bdaf0573d985a0e
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-15 10:11:37 +0000

    Fix code

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -4,14 +4,15 @@
  * free_dog - Entry point
  *
  * Description: a function that frees dogs
- * @d: type dog
- *
- * Return: 
+ * @d: type dog 
  */
 
 void free_dog(dog_t *d)
 {
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
