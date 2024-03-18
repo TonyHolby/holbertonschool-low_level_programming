a4507bf8a30ede5d90fa8cc9e2405a0dfc2eecd5
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-18 14:58:23 +0000

    Fix code

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -20,17 +20,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	while (i <= size && cmp(array[i]) == 0)
+	for (i = 0; i < size; i++)
 	{
-
-		if (i < size)
+		if (cmp(array[i]) != 0)
 		{
-			i++;
-		}
-		else
-		{
-			return (-1);
+			return (i);
 		}
 	}
-	return (i);
+
+	return (-1);
 }
