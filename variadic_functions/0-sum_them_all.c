072345c117406a4c1cea47ebd5eac3c69c73c0f1
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-20 09:38:30 +0000

    Fix code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,17 +15,18 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int res = 0;
 
+	va_start(nb_args, n);
+
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	va_start(nb_args, n);
-
 	for (i = 0; i < n; i++)
 	{
 		res += va_arg(nb_args, int);
 	}
+
 	va_end(nb_args);
 
 	return (res);
