6cf4ba0557542cd9b51a795f58debe0f8c59bd82
TonyHolby <tonynemoutheholberton@gmail.com>
2024-03-31 20:39:48 +0000

    Fix betty style and betty doc

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: pointer to the singly linked list node structure
+ * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	const list_t *current = h;
@@ -23,7 +24,7 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", current->len, current->str);
 			number_of_nodes++;
 		}
-                current = current->next;
-        }
-        return (number_of_nodes);
+		current = current->next;
+	}
+	return (number_of_nodes);
 }
