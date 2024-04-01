45416d04257177dc91738ad490c914739ced999d
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-01 10:29:18 +0000

    Fix code in the if condition in 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,9 +15,7 @@ size_t print_list(const list_t *h)
 	{
 		if (current->str == NULL)
 		{
-			printf("[0] ");
-			printf("(nil)\n");
-			number_of_nodes++;
+			printf("[0] (nil)\n");
 		}
 		else
 		{
