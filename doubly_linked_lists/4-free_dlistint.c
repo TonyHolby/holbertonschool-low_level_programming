7f4c51bbac250d2850a7728b92afd30260a532d8
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-03 11:49:26 +0000

    Create the 4-free_dlistint.c file

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - a function that frees a dlistint_t list.
+ * @head: pointer to the singly linked list node structure
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->prev;
+
+		free(head->next);
+
+		free(head);
+
+		head = next_node;
+	}
+}	
