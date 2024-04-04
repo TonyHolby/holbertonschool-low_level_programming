d3e3ac49742e78232f6f85f0dd782c6857e61e6d
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-04 19:24:37 +0000

    Fix betty style in 7-insert_dnodeint.c file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,55 +11,43 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
 	dlistint_t *current, *new_node;
 
 	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (idx == 0)
 	{
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
+	current = *h;
+	unsigned int = 0;
+
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+	if (current == NULL)
+	{
+		free(new_node);
 		return (NULL);
 	}
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 
-	 new_node = malloc(sizeof(dlistint_t));
-	 {
-		 return (NULL);
-	 }
-
-	 new_node->n = n;
-	 new_node->prev = NULL;
-	 new_node->next = NULL;
-
-	 if (idx == 0)
-	 {
-		 new_node->next = *h;
-		 if (*h != NULL)
-		 {
-			 (*h)->prev = new_node;
-		 }
-		 *h = new_node;
-		 return (new_node);
-	 }
-
-	 current = *h;
-	 i = 0;
-	 while (current != NULL && i < idx - 1)
-	 {
-		 current = current->next;
-		 i++;
-	 }
-
-	 if (current == NULL)
-	 {
-		 free(new_node);
-		 return (NULL);
-	 }
-
-	 new_node->next = current->next;
-	 new_node->prev = current;
-	 if (current->next != NULL)
-	 {
-		 current->next->prev = new_node;
-	 }
-	 current->next = new_node;
-
-	 return (new_node);
+	return (new_node);
 }
