1f278a94873ab89533ca7aa4fdafb3c4226a70c7
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-06 17:00:51 +0000

    Create the main.h file

---
+++ file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef main_h
+#define main_h
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
