f1b5a7da2a298a374d546a9f5906276c69642cf5
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-06 19:57:21 +0000

    Update the main.h file

--- file_io/main.h
+++ file_io/main.h
@@ -7,7 +7,10 @@
 #include <sys/stat.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
